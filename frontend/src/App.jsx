01ae449cd30641eb68372e39869748c249dee80f
samikappanna <106487728+samikappanna@users.noreply.github.com>
2025-02-12 02:32:45 +0530

    theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,11 @@ import HomePage from './pages/HomePage'
 import { useAuthStore } from './store/useAuthStore'
 import {Loader} from 'lucide-react'
 import {Toaster} from 'react-hot-toast'
+import { useThemeStore } from './store/useThemeStore'
 
 const App = () => {
   const {authUser,checkAuth,isCheckingAuth} = useAuthStore()  
+  const {theme}=useThemeStore()
   
   useEffect(() => {
     
@@ -27,7 +29,7 @@ const App = () => {
   
 
   return (
-    <div datat-theme="retro">
+    <div data-theme={theme}>
       <Navbar/>
 
       <Routes>
